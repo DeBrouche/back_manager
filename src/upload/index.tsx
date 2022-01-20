e605c7d18735f331e0168a77f591eaff3129b15c
Mingyingjie <mingyingjie@qq.com>
2022-01-20 22:25:25 +0800

    deploy

--- src/upload/index.tsx
+++ src/upload/index.tsx
@@ -1,23 +1,43 @@
 import InspireCloud from "@byteinspire/js-sdk";
 import axios from "axios";
-import { Component } from "react";
+import React, { Component } from "react";
 
 import "./index.css";
 
+const serviceId = "qcdnq7"; // 替换成你的 serviceId，可在后台「设置」页面获取
+const fileUploadToken = "4b7e46f0-1a37-4337-8e18-2bc9acef2e24"; // 替换成你的客户端上传 token，可在后台「设置」页面获取
+const inspirecloud = new InspireCloud({ serviceId });
+
 class UpLoad extends Component {
+  plugname_input = React.createRef<HTMLInputElement>();
+  id_input = React.createRef<HTMLInputElement>();
+  plugdes_input = React.createRef<HTMLTextAreaElement>();
   state = {
     url: "",
     uid: "",
     thumnail_url: "",
     thumnail_name: "",
     thumnail_size: "",
+    plug_name: "",
+    plug_des: "",
+    thumnail_file: "",
+    scripts_file: "",
+    id: "",
+  };
+  setscripts = () => {
+    const fileInputElement = document.getElementById(
+      "fileInput"
+    ) as HTMLInputElement;
+    if (fileInputElement.files) {
+      if (fileInputElement.files.length > 0) {
+        const file = fileInputElement.files[0];
+        this.setState({
+          scripts_file: file,
+        });
+      }
+    }
   };
   upLoadFiles = () => {
-    const serviceId = "qcdnq7"; // 替换成你的 serviceId，可在后台「设置」页面获取
-    const fileUploadToken = "4b7e46f0-1a37-4337-8e18-2bc9acef2e24"; // 替换成你的客户端上传 token，可在后台「设置」页面获取
-
-    const inspirecloud = new InspireCloud({ serviceId });
-
     // 获取到上传选择框
     const fileInputElement = document.getElementById(
       "fileInput"
@@ -25,6 +45,8 @@ class UpLoad extends Component {
     if (fileInputElement.files) {
       if (fileInputElement.files.length > 0) {
         const file = fileInputElement.files[0];
+
+        // 最早的上传路线
         const filename = file.name;
 
         // 调用 JavaScript SDK 的 upload 方法实现上传，第二个参数为 File 对象
@@ -68,29 +90,80 @@ class UpLoad extends Component {
       let { name, size } = fir_input.files[0];
       console.log(url, name, size);
       thiscom.setState({
+        thumnail_file: fir_input.files[0],
         thumnail_url: url,
         thumnail_name: name,
         thumnail_size: size,
       });
     }
   };
+  setname = () => {
+    let nameinput = this.plugname_input.current;
+    if (nameinput) {
+      console.log(nameinput.value);
+      this.setState({
+        plug_name: nameinput.value,
+      });
+    }
+  };
+  upload_plugin = () => {
+    console.log(this.state);
+    const formData = new FormData();
+    formData.append("plug_name", this.state.plug_name);
+    formData.append("plug_des", this.state.plug_des);
+    formData.append("plug_thumnail", this.state.thumnail_file);
+    formData.append("thumnail_name", this.state.thumnail_name);
+    formData.append("scripts_file", this.state.scripts_file);
+    formData.append("uid", this.state.id);
+    // console.log(formData);
+    inspirecloud
+      .run("upload_plugin", formData, {
+        headers: {
+          "content-type": "multipart/form-data",
+        },
+      })
+      .then((res) => console.log(res));
+  };
+  showstate = () => {
+    console.log(this.state);
+  };
   componentDidMount() {
     this.setState({
       uid: "712931055244973",
     });
   }
-
+  setid = () => {
+    let input = this.id_input.current;
+    if (input) {
+      this.setState({
+        id_input: input.value,
+      });
+    }
+  };
+  setdes = () => {
+    let desinput = this.plugdes_input.current;
+    if (desinput) {
+      console.log(desinput.value);
+      this.setState({ plug_des: desinput.value });
+    }
+  };
   render() {
     return (
       <div className="inputs-wrapper">
         {/* 名称 */}
         <div className="des-input ">
           <div className="input-title">插件名称</div>
+
+          <input
+            ref={this.plugname_input}
+            type="text"
+            onChange={this.setname}
+          />
         </div>
         {/* 脚本 */}
         <div className="testinput">
           <div className="input-title">js脚本</div>
-          <input type="file" id="fileInput" />
+          <input onChange={this.setscripts} type="file" id="fileInput" />
           <button onClick={this.upLoadFiles}>上传文件</button>
         </div>
         {/* 头像 */}
@@ -131,13 +204,31 @@ class UpLoad extends Component {
           </div>
           <div className="add-name">
             <p>点击选择图片</p>
-            <p>{}</p>
+            <p>{this.state.thumnail_name}</p>
           </div>
         </div>
         {/* 描述 */}
         <div className="des-input">
           <div className="input-title">插件描述</div>
+          <div className="des-box">
+            <textarea
+              ref={this.plugdes_input}
+              onChange={this.setdes}
+              name="plug_des"
+              id="des_input"
+              cols={70}
+              rows={5}
+            ></textarea>
+            <input
+              type="text"
+              ref={this.id_input}
+              onChange={this.setid}
+              placeholder="设置用户id"
+            />
+          </div>
         </div>
+        <button onClick={this.showstate}>显示数据</button>
+        <button onClick={this.upload_plugin}>上传插件</button>
       </div>
     );
   }
